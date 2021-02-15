93fbf216fa4f258edc4ba145ba3d64bcb1223fb5
Brian Do <briancldo@gmail.com>
2021-02-15 16:38:19 -0500

    attempts to implement interest rate formula with recurring deposits

---
+++ src/utils/interest.js
@@ -0,0 +1,48 @@
+import { sigma } from './math';
+import { DevError } from './errors';
+
+const frequencies = {
+  month: 12,
+  quarter: 4,
+  annual: 1,
+};
+const durations = {
+  month: 1,
+  quarter: 1,
+  year: 1,
+};
+function validateCalculateInvestment(contribution, compound) {
+  if (typeof contribution.principal !== 'number') throw new DevError('Principal should be number.');
+  if (!(contribution.principal > 0)) throw new DevError('Principal must be a positive number.');
+  if (typeof contribution !== 'object' || contribution === null)
+    throw new DevError('Contrution must be an object.');
+  if (!frequencies[contribution.frequency])
+    throw new DevError(`Invalid contribution frequency: "${contribution.frequency}".`);
+  if (typeof contribution.amount !== 'number')
+    throw new DevError('Contribution amount should be a number.');
+  if (!durations[contribution.duration])
+    throw new DevError(`Invalid duration: "${contribution.duration}".`);
+  
+  if (typeof compound.rate !== 'number') throw new DevError('Rate should be a number.');
+  if (!(compound.rate > 0 && compound.rate < 1)) throw new DevError('Rate should be between 0% and 100%');
+  if (!frequencies[compound.frequency])
+    throw new DevError(`Invalid compound frequency: "${compound.frequency}".`);
+}
+
+function calculateContributionForPeriod(compoundFrequency, contributionFrequency, contributionAmount) {
+  return (contributionFrequency/compoundFrequency) * contributionAmount;
+}
+
+function interestSigmaFormula(i, constants) {
+  const { rate, periodContribution } = constants;
+  return (rate**i) * periodContribution;
+}
+
+export function calculateInvestment(contribution, compound) {
+  validateCalculateInvestment(contribution, rate);
+  const periodContribution = calculateContributionForPeriod(compound.frequency, contribution.frequency, contribution.amount);
+  const _rate = 1 + rate;
+
+  const interestSum = sigma(1, contribution.duration, interestSigmaFormula, { rate: _rate, periodContribution });
+  return interestSum + ( (_rate**contribution.duration) * contribution.principal );
+}
---
+++ src/utils/math.js
@@ -0,0 +1,9 @@
+export function sigma(from, to, formula, constants) {
+  let sum = 0;
+
+  for (let i = from; i <= to; i++) {
+    formula(i, constants);
+  }
+
+  return sum;
+}
